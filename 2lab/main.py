a924c39f1afe43d6820e6354f0882a28191f2c1c
DarkBall123 <vitalya228007@gmail.com>
2025-03-14 12:35:53 +0300

    Обновление лаб. 1 + 2

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,7 +1,13 @@
-def main():
-    # Ваш код здесь
-    pass
+import subprocess
+import uvicorn
+def run_migrations():
+    print("alembic")
+    subprocess.run(["alembic", "upgrade", "head"])
+
+def run_server():
+    print("uvicorn")
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 if __name__ == "__main__":
-    main()
-
+    run_migrations()
+    run_server()
